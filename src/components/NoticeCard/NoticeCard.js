bb6c69fa655d2180a4d19c5e0fc4e62886b0debf
LouisDC98 <louisdacosta98@gmail.com>
2021-08-13 11:18:12 +0200

    Add notice card component

---
+++ src/components/NoticeCard/NoticeCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './NoticeCard.css';
+import { withRouter } from 'react-router';
+
+class NoticeCard extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.props.history.push('/rent/' + this.props.notice.id);
+    }
+
+    render() {
+        return (
+            <figure className="card one" onClick={this.handleClick}>
+                <img className="imgCard" src={this.props.notice.cover} alt="Notice 1" />
+                <figcaption className="textCard">{this.props.notice.title}</figcaption>
+            </figure>
+        );
+    }
+}
+
+export default withRouter(NoticeCard);
