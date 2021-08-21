570dc8ef37f666f3bd60a9bbb1154df4111ca619
LouisDC98 <louisdacosta98@gmail.com>
2021-08-21 18:55:29 +0200

    Upgrade import system

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Slogan from '../../components/Slogan/Slogan';
-import NoticeCard from '../../components/NoticeCard/NoticeCard';
+import Slogan from 'components/Slogan/Slogan';
+import NoticeCard from 'components/NoticeCard/NoticeCard';
 import './Home.css';
-import { getAllNotices } from '../../datas/fakeAPI';
+import { getAllNotices } from 'datas/fakeAPI';
 
 class Home extends React.Component {
     render() {
