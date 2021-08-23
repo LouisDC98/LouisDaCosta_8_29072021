70c07223b658ea7e88ef3f6eefe2baf0eedefe5a
LouisDC98 <louisdacosta98@gmail.com>
2021-08-23 16:09:45 +0200

    Update React import

--- src/components/NoticeCard/NoticeCard.js
+++ src/components/NoticeCard/NoticeCard.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './NoticeCard.css';
 import { withRouter } from 'react-router';
 
-class NoticeCard extends Component {
+class NoticeCard extends React.Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
