b2e6328ef42e27e1ecee7d83b322d433548c353c
LouisDC98 <louisdacosta98@gmail.com>
2021-08-23 15:10:12 +0200

    Update About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
+import Accordion from 'components/Accordion/Accordion';
+import Slogan from 'components/Slogan/Slogan';
+import backgroundAbout from 'img/backgroundAbout.png';
 
 class About extends Component {
     render() {
-        return <div>About page</div>;
+        return (
+            <>
+                <Slogan img={backgroundAbout} text="" />
+                <div className="accordionElement">
+                    <Accordion
+                        title={'Fiabilité'}
+                        content={
+                            'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.'
+                        }
+                    />
+                </div>
+                <div className="accordionElement">
+                    <Accordion
+                        title={'Respect'}
+                        content={
+                            'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
+                        }
+                    />
+                </div>
+                <div className="accordionElement">
+                    <Accordion
+                        title={'Service'}
+                        content={
+                            "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+                        }
+                    />
+                </div>
+                <div className="accordionElement">
+                    <Accordion
+                        title={'Responsabilité'}
+                        content={
+                            "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+                        }
+                    />
+                </div>
+            </>
+        );
     }
 }
 
