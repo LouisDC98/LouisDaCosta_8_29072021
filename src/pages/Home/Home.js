958941f009414dd30826e6c96be958ff5d7e8db1
LouisDC98 <louisdacosta98@gmail.com>
2021-08-23 14:52:14 +0200

    Update img import system

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,13 +3,14 @@ import Slogan from 'components/Slogan/Slogan';
 import NoticeCard from 'components/NoticeCard/NoticeCard';
 import './Home.css';
 import { getAllNotices } from 'datas/fakeAPI';
+import background from 'img/background.png';
 
 class Home extends React.Component {
     render() {
         const dataArray = getAllNotices();
         return (
             <>
-                <Slogan img="./background.png" text="Chez vous, partout et ailleurs" />
+                <Slogan img={background} text="Chez vous, partout et ailleurs" />
                 <div className="bg wrapper">
                     {dataArray.map((data) => (
                         <NoticeCard key={data.id} notice={data} />
