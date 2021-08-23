4201506dfa865647d59c2b6220502013d895d6ff
LouisDC98 <louisdacosta98@gmail.com>
2021-08-23 14:41:33 +0200

    Move router in app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Router from './pages/Router';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import Footer from './components/Footer/Footer';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Rent from './pages/Rent/Rent';
+import Error from './pages/Error/Error';
 
 function App() {
     return (
@@ -11,7 +15,20 @@ function App() {
             <div className="bodyShape">
                 <Header />
                 <div className="body">
-                    <Router />
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route exact path="/rent/:id">
+                            <Rent />
+                        </Route>
+                        <Route path="*">
+                            <Error />
+                        </Route>
+                    </Switch>
                 </div>
             </div>
             <Footer />
