76c2cd7e0a846c1cf6382a3f64c939d58367d34f
LouisDC98 <louisdacosta98@gmail.com>
2021-09-06 12:21:08 +0200

    Update Router file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Header from './components/Header/Header';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import Footer from './components/Footer/Footer';
-import { Switch, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Rent from './pages/Rent/Rent';
-import Error from './pages/Error/Error';
+import Router from 'Router';
 
 function App() {
     return (
@@ -15,20 +11,7 @@ function App() {
             <div className="bodyShape">
                 <Header />
                 <div className="body">
-                    <Switch>
-                        <Route exact path="/">
-                            <Home />
-                        </Route>
-                        <Route path="/about">
-                            <About />
-                        </Route>
-                        <Route exact path="/rent/:id">
-                            <Rent />
-                        </Route>
-                        <Route path="*">
-                            <Error />
-                        </Route>
-                    </Switch>
+                    <Router />
                 </div>
             </div>
             <Footer />
